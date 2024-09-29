660e43663ac52bb128cc77a479f6554a01247e7f
Sill <158129203+Silly-String@users.noreply.github.com>
2024-09-29 04:20:07 -0400

    Completed TODOs in JSONTranslator.java

--- src/main/java/org/translation/JSONTranslator.java
+++ src/main/java/org/translation/JSONTranslator.java
@@ -5,9 +5,12 @@ import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.json.JSONArray;
+import org.json.JSONObject;
 
 /**
  * An implementation of the Translator interface which reads in the translation
@@ -15,7 +18,10 @@ import org.json.JSONArray;
  */
 public class JSONTranslator implements Translator {
 
-    // TODO Task: pick appropriate instance variables for this class
+    // Maps country code to a map of language code to translation
+    private Map<String, Map<String, String>> countryTranslations;
+    // List of country codes
+    private List<String> countryCodes;
 
     /**
      * Constructs a JSONTranslator using data from the sample.json resources file.
@@ -30,6 +36,10 @@ public class JSONTranslator implements Translator {
      * @throws RuntimeException if the resource file can't be loaded properly
      */
     public JSONTranslator(String filename) {
+
+        countryTranslations = new HashMap<>();
+        countryCodes = new ArrayList<>();
+
         // read the file to get the data to populate things...
         try {
 
@@ -37,9 +47,22 @@ public class JSONTranslator implements Translator {
 
             JSONArray jsonArray = new JSONArray(jsonString);
 
-            // TODO Task: use the data in the jsonArray to populate your instance variables
-            //            Note: this will likely be one of the most substantial pieces of code you write in this lab.
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject countryObject = jsonArray.getJSONObject(i);
+                String alpha3 = countryObject.getString("alpha3");
+                Map<String, String> translations = new HashMap<>();
 
+                // Populate translations for the current country
+                for (String languageCode : countryObject.keySet()) {
+                    if (!"id".equals(languageCode) && !"alpha2".equals(languageCode)
+                            && !"alpha3".equals(languageCode)) {
+                        translations.put(languageCode, countryObject.getString(languageCode));
+                    }
+                }
+
+                countryTranslations.put(alpha3, translations);
+                countryCodes.add(alpha3);
+            }
         }
         catch (IOException | URISyntaxException ex) {
             throw new RuntimeException(ex);
@@ -48,21 +71,30 @@ public class JSONTranslator implements Translator {
 
     @Override
     public List<String> getCountryLanguages(String country) {
-        // TODO Task: return an appropriate list of language codes,
-        //            but make sure there is no aliasing to a mutable object
-        return new ArrayList<>();
+        // Retrieve the map of language codes for the given country, or an empty map if not found
+        Map<String, String> translations = countryTranslations.getOrDefault(country, new HashMap<>());
+
+        // Return a new ArrayList of the language codes (keys)
+        return new ArrayList<>(translations.keySet());
     }
 
     @Override
     public List<String> getCountries() {
-        // TODO Task: return an appropriate list of country codes,
-        //            but make sure there is no aliasing to a mutable object
-        return new ArrayList<>();
+        return new ArrayList<>(countryCodes);
     }
 
     @Override
     public String translate(String country, String language) {
-        // TODO Task: complete this method using your instance variables as needed
+        Map<String, String> translations = countryTranslations.get(country);
+
+        // Check if the translations map is not null
+        if (translations != null) {
+            // Retrieve the translation for the specified language
+            String translation = translations.get(language);
+            // Return the translation if found, or null if not
+            return translation;
+        }
+        // Return null if no translations are found for the specified country
         return null;
     }
 }
