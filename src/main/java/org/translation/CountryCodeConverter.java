3b8c63efffeb718e265dd63bd8b77e5d0f525460
Sill <158129203+Silly-String@users.noreply.github.com>
2024-09-29 00:06:49 -0400

    Completed TODOs in CountryCodeConverter.java

--- src/main/java/org/translation/CountryCodeConverter.java
+++ src/main/java/org/translation/CountryCodeConverter.java
@@ -13,7 +13,15 @@ import java.util.Map;
  */
 public class CountryCodeConverter {
 
-    // TODO Task: pick appropriate instance variable(s) to store the data necessary for this class
+    // Java is being stupid and won't let me type the numbre 3
+    private static final int MY_NUMBER = 3;
+
+    // For Alpha-3 code to Country name
+    private Map<String, String> codeToCountryMap;
+    // For Country name to Alpha-3 code
+    private Map<String, String> countryToCodeMap;
+    // For tracking the number of countries
+    private int numCountries;
 
     /**
      * Default constructor which will load the country codes from "country-codes.txt"
@@ -28,13 +36,31 @@ public class CountryCodeConverter {
      * @param filename the name of the file in the resources folder to load the data from
      * @throws RuntimeException if the resource file can't be loaded properly
      */
+
     public CountryCodeConverter(String filename) {
+
+        codeToCountryMap = new HashMap<>();
+        countryToCodeMap = new HashMap<>();
 
         try {
             List<String> lines = Files.readAllLines(Paths.get(getClass()
                     .getClassLoader().getResource(filename).toURI()));
 
-            // TODO Task: use lines to populate the instance variable(s)
+            // Process each line (skip the header)
+            for (int i = 1; i < lines.size(); i++) {
+                String[] parts = lines.get(i).split("\t");
+                if (parts.length >= MY_NUMBER) {
+                    String countryName = parts[0];
+                    String alpha3Code = parts[2];
+
+                    // Populate maps
+                    codeToCountryMap.put(alpha3Code, countryName);
+                    countryToCodeMap.put(countryName, alpha3Code);
+                }
+            }
+
+            // Total number of countries
+            numCountries = codeToCountryMap.size();
 
         }
         catch (IOException | URISyntaxException ex) {
@@ -49,8 +75,8 @@ public class CountryCodeConverter {
      * @return the name of the country corresponding to the code
      */
     public String fromCountryCode(String code) {
-        // TODO Task: update this code to use an instance variable to return the correct value
-        return code;
+
+        return codeToCountryMap.get(code);
     }
 
     /**
@@ -59,8 +85,8 @@ public class CountryCodeConverter {
      * @return the 3-letter code of the country
      */
     public String fromCountry(String country) {
-        // TODO Task: update this code to use an instance variable to return the correct value
-        return country;
+
+        return countryToCodeMap.get(country);
     }
 
     /**
@@ -68,7 +94,7 @@ public class CountryCodeConverter {
      * @return how many countries are included in this code converter.
      */
     public int getNumCountries() {
-        // TODO Task: update this code to use an instance variable to return the correct value
-        return 0;
+
+        return numCountries;
     }
 }
